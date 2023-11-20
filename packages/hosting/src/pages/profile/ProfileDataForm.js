59422cc6987ff3f4ad67009fc0453bba1cb239f2
servitecperu266 <servitecperu266@gmail.com>
2023-11-20 18:20:40 -0500

    add profile form

--- packages/hosting/src/pages/profile/ProfileDataForm.js
+++ packages/hosting/src/pages/profile/ProfileDataForm.js
@@ -1,21 +1,99 @@
 import React from "react";
-import { Button, Form, Input } from "../../components";
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
+import { yupResolver } from "@hookform/resolvers/yup/dist/yup";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { useFormUtils } from "../../hooks";
+import { Button, Form, Input } from "../../components";
 
 export const ProfileDataForm = () => {
+  const schema = yup.object({
+    name: yup.string().required(),
+    fatherLastName: yup.string().required(),
+    motherLastName: yup.string().required(),
+    email: yup.string().email().required(),
+  });
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const { required, error, errorMessage } = useFormUtils({ errors, schema });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <Row gutter={[16, 16]}>
         <Col span={24}>
-          <Input label="Nombres" />
+          <Controller
+            name="name"
+            control={control}
+            render={({ field: { onChange, name, value } }) => (
+              <Input
+                label="Nombres"
+                name={name}
+                onChange={onChange}
+                value={value}
+                error={error(name)}
+                helperText={errorMessage(name)}
+              />
+            )}
+          />
         </Col>
         <Col span={24} md={12}>
-          <Input label="Apellido paterno" />
+          <Controller
+            name="fatherLastName"
+            control={control}
+            render={({ field: { onChange, name, value } }) => (
+              <Input
+                label="Apellido paterno"
+                name={name}
+                onChange={onChange}
+                value={value}
+                error={error(name)}
+                helperText={errorMessage(name)}
+              />
+            )}
+          />
         </Col>
 
         <Col span={24} md={12}>
-          <Input label="Apellido materno" />
+          <Controller
+            name="motherLastName"
+            control={control}
+            render={({ field: { onChange, name, value } }) => (
+              <Input
+                label="Apellido materno"
+                name={name}
+                onChange={onChange}
+                value={value}
+                error={error(name)}
+                helperText={errorMessage(name)}
+              />
+            )}
+          />
+        </Col>
+        <Col span={24}>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field: { onChange, name, value } }) => (
+              <Input
+                label="Email"
+                name={name}
+                onChange={onChange}
+                value={value}
+                error={error(name)}
+                helperText={errorMessage(name)}
+              />
+            )}
+          />
         </Col>
       </Row>
 
