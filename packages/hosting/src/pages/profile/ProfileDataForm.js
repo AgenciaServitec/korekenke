b37b90f42b09fb24a08384d0d642bcb6e51c8c07
servitecperu266 <servitecperu266@gmail.com>
2023-11-21 16:39:44 -0500

    show data form

--- packages/hosting/src/pages/profile/ProfileDataForm.js
+++ packages/hosting/src/pages/profile/ProfileDataForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
 import { yupResolver } from "@hookform/resolvers/yup/dist/yup";
@@ -6,12 +6,15 @@ import { Controller, useForm } from "react-hook-form";
 import * as yup from "yup";
 import { useFormUtils } from "../../hooks";
 import { Button, Form, Input } from "../../components";
+import { useAuthentication } from "../../providers";
 
 export const ProfileDataForm = () => {
+  const { authUser } = useAuthentication();
+
   const schema = yup.object({
     name: yup.string().required(),
-    fatherLastName: yup.string().required(),
-    motherLastName: yup.string().required(),
+    paternalSurname: yup.string().required(),
+    maternalSurname: yup.string().required(),
     email: yup.string().email().required(),
   });
 
@@ -19,13 +22,29 @@ export const ProfileDataForm = () => {
     handleSubmit,
     control,
     formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const { error, errorMessage } = useFormUtils({ errors, schema });
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = (formData) => {
+    console.log("formData->", formData);
   };
+
+  const resetForm = () => {
+    reset({
+      name: authUser?.firstName || "",
+      maternalSurname: authUser?.maternalSurname || "",
+      paternalSurname: authUser?.paternalSurname || "",
+      email: authUser?.email || "",
+    });
+  };
+
+  useEffect(() => {
+    resetForm();
+  }, [authUser]);
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
@@ -48,7 +67,7 @@ export const ProfileDataForm = () => {
         </Col>
         <Col span={24} md={12}>
           <Controller
-            name="fatherLastName"
+            name="paternalSurname"
             control={control}
             render={({ field: { onChange, name, value } }) => (
               <Input
@@ -65,7 +84,7 @@ export const ProfileDataForm = () => {
 
         <Col span={24} md={12}>
           <Controller
-            name="motherLastName"
+            name="maternalSurname"
             control={control}
             render={({ field: { onChange, name, value } }) => (
               <Input
