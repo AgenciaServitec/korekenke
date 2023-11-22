be9967109b94afe255f0faef92b5ffc46fc9109f
noelMoriano <nmoriano26@gmail.com>
2023-11-22 01:48:33 -0500

    Add useApiCorrespondencePost

--- packages/hosting/src/api/index.js
+++ packages/hosting/src/api/index.js
@@ -3,3 +3,4 @@ export * from "./useApiUserPost";
 export * from "./useApiUserPut";
 export * from "./useApiUserPatch";
 export * from "./useApiSearchGet";
+export * from "./useApiCorrespondencePost";
