b67056a8ae456f26f761254fb64c3e4822c33a3b
Angel gt <87275731+Angelgamonal@users.noreply.github.com>
2023-12-27 19:10:02 -0500

    Upload image (#21)
    * add Upload component
    
    * add error and helperText in Upload

--- packages/hosting/src/pages/profile/ProfileImagesForm.js
+++ packages/hosting/src/pages/profile/ProfileImagesForm.js
@@ -1,18 +1,90 @@
 import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
-import { Button, Form } from "../../components";
+import { Button, Form, Upload } from "../../components";
+import { useForm, Controller } from "react-hook-form";
 
 export const ProfileImagesForm = () => {
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const schema = yup.object({
+    dniPhoto: yup.mixed().required(),
+    cipPhoto: yup.mixed().required(),
+    signaturePhoto: yup.mixed().required(),
+  });
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onHandleSubmit = (e) => {
     console.log(e.target);
   };
 
   return (
-    <Form onSubmit={onSubmit}>
-      <Row></Row>
+    <Form onSubmit={handleSubmit(onHandleSubmit)}>
       <Row justify="end" gutter={[16, 16]}>
+        <Col xs={24} sm={12} md={12}>
+          <Controller
+            control={control}
+            name="dniPhoto"
+            render={({ field: { onChange, value, onBlur, name } }) => (
+              <Upload
+                label="Foto de DNI"
+                accept="image/*"
+                resize="400x400"
+                buttonText="Subir foto"
+                onChange={(file) => onChange(file)}
+                value={value}
+                name={name}
+                error={errors[name]}
+                helperText={errors[name]?.message}
+              />
+            )}
+          />
+        </Col>
+        <Col xs={24} sm={12} md={12}>
+          <Controller
+            control={control}
+            name="cipPhoto"
+            render={({ field: { onChange, value, onBlur, name } }) => (
+              <Upload
+                label="Foto de CIP"
+                accept="image/*"
+                resize="400x400"
+                buttonText="Subir foto"
+                onChange={(file) => onChange(file)}
+                value={value}
+                name={name}
+                error={errors[name]}
+                helperText={errors[name]?.message}
+              />
+            )}
+          />
+        </Col>
+        <Col span={24}>
+          <Controller
+            control={control}
+            name="signaturePhoto"
+            render={({ field: { onChange, value, onBlur, name } }) => (
+              <Upload
+                label="Foto de firma"
+                accept="image/*"
+                resize="400x400"
+                buttonText="Subir foto"
+                onChange={(file) => onChange(file)}
+                value={value}
+                name={name}
+                error={errors[name]}
+                helperText={errors[name]?.message}
+              />
+            )}
+          />
+        </Col>
         <Col xs={24} sm={12} md={8}>
           <Button type="primary" size="large" block htmlType="submit">
             Guardar
