f513535ecdf5570ffe77a7101bfe698d3141b74c
Ignacio2906 <145935490+Ignacio2906@users.noreply.github.com>
2024-04-26 17:05:16 -0500

    add inscription sheet (#25)
    * add inscription sheet
    
    * format code
    
    * updated workflow
    
    * format code
    
    ---------
    
    Co-authored-by: Pablo Aquise Lozano <aquise.pablo@iestpgildaballivian.edu.pe>
    Co-authored-by: noel <admin@agenciaservitec.com>

--- packages/hosting/src/router/index.js
+++ packages/hosting/src/router/index.js
@@ -3,7 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import { AdminLayout } from "../components/layout/AdminLayout";
 import { PrivateRoute } from "./PrivateRoute";
 import * as A from "../pages";
-import { LoginIntegration, RegisterIntegration } from "../pages";
+import {
+  InscriptionFile,
+  LoginIntegration,
+  RegisterIntegration,
+} from "../pages";
 import { Page404 } from "../pages/404";
 
 export const Router = () => {
@@ -65,8 +69,12 @@ export const Router = () => {
               <A.Profile />
             </AdminLayout>
           }
+        />{" "}
+        <Route
+          exact
+          path="inscriptions/cmsts/sheet"
+          element={<A.InscriptionFile />}
         />
-
         {/*<Route*/}
         {/*  exact*/}
         {/*  path="scripts"*/}
