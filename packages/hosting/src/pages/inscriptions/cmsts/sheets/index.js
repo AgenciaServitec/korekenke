80fbeab51908adc793ce580b84c486962217758f
Angel Gala <galafloresangelemilio@gmail.com>
2024-04-30 12:08:22 -0500

    refactor code

--- packages/hosting/src/pages/inscriptions/cmsts/sheets/index.js
+++ packages/hosting/src/pages/inscriptions/cmsts/sheets/index.js
@@ -1,15 +1,8 @@
 import React from "react";
-import styled from "styled-components";
 import { PDF } from "./PDF";
-import Col from "antd/lib/col";
-import Row from "antd/lib/row";
 import { PdfRegistrationCmsts } from "./PdfRegistrationCmsts";
 import { Sheet } from "./Sheet";
 
-const PAGE_SIZES = {
-  portrait: { width: "210mm", height: "297mm" },
-  landscape: { width: "297mm", height: "210mm" },
-};
 export const InscriptionFile = () => {
   return (
     <PDF>
@@ -19,17 +12,3 @@ export const InscriptionFile = () => {
     </PDF>
   );
 };
-
-const Container = styled(Row)`
-  padding: 0 1em;
-  margin: 1em 0;
-  .item-button {
-    @media print {
-      display: none;
-    }
-  }
-
-  .background-yellow {
-    background-color: #ccc;
-  }
-`;
