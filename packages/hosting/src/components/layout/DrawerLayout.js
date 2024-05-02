030e8b7d19af34b08fa58e3049b9557a7b4bd2a9
Angel Gala <galafloresangelemilio@gmail.com>
2024-05-02 17:03:25 -0500

    refactoring imports

--- packages/hosting/src/components/layout/DrawerLayout.js
+++ packages/hosting/src/components/layout/DrawerLayout.js
@@ -11,7 +11,7 @@ import {
   faUsersCog,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { includes, isEmpty } from "lodash";
+import { includes } from "lodash";
 
 export const DrawerLayout = ({
   user,
