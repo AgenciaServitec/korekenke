a8453556b89c4ffd144f30aab7b2ccc7a9c0154e
noel <admin@agenciaservitec.com>
2024-05-15 16:55:20 -0500

    Added updateUsersWithBatch

--- packages/hosting/src/firebase/collections/users.js
+++ packages/hosting/src/firebase/collections/users.js
@@ -1,5 +1,6 @@
 import { firestore } from "../index";
 import { fetchCollectionOnce, fetchDocumentOnce } from "../utils";
+import { useDefaultFirestoreProps } from "../../hooks";
 
 export const usersRef = firestore.collection("users");
 
@@ -9,3 +10,18 @@ export const fetchUser = async (id) => fetchDocumentOnce(usersRef.doc(id));
 
 export const fetchUsers = async () =>
   fetchCollectionOnce(usersRef.where("isDeleted", "==", false));
+
+export const updateUsersWithBatch = async (users = []) => {
+  const { assignUpdateProps } = useDefaultFirestoreProps();
+
+  const batch = firestore.batch();
+
+  users.forEach((user) =>
+    batch.update(
+      firestore.collection("users").doc(user.id),
+      assignUpdateProps(user)
+    )
+  );
+
+  return await batch.commit();
+};
