00dc5e7ef9acfb4789e248f90d316f1fcad9b042
Angel Gala <galafloresangelemilio@gmail.com>
2024-07-16 18:11:34 -0500

    fixed signature component

--- packages/hosting/src/pages/entities/departamento-de-apoyo-social/das-requests/sheets/StudyScholarshipAwardedByUniversity.jsx
+++ packages/hosting/src/pages/entities/departamento-de-apoyo-social/das-requests/sheets/StudyScholarshipAwardedByUniversity.jsx
@@ -80,7 +80,7 @@ export const StudyScholarshipAwardedByUniversitySheet = ({
               </p>
               <SignatureSheet
                 signaturethumbUrl={headline?.signaturePhoto?.thumbUrl}
-                signatureUrl={headline.signaturePhoto.url}
+                signatureUrl={headline?.signaturePhoto.url}
                 name={userFullName(headline)}
                 cip={headline?.cip}
                 degree={findDegree(headline?.degree)?.label}
