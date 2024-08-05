21896cb2bc5762b1e01dd1100e7e884cdb9b3785
noel <admin@agenciaservitec.com>
2024-08-05 16:00:33 -0500

    Updated addAnimalLog

--- packages/hosting/src/pages/entities/servicio-de-veterinaria-y-remonta-del-ejercito/animals/_animalId/index.jsx
+++ packages/hosting/src/pages/entities/servicio-de-veterinaria-y-remonta-del-ejercito/animals/_animalId/index.jsx
@@ -28,7 +28,6 @@ import {
   addAnimalLog,
   getAnimalId,
   updateAnimal,
-  updateAnimalLog,
 } from "../../../../../firebase/collections";
 import dayjs from "dayjs";
 import { v4 as uuidv4 } from "uuid";
@@ -64,6 +63,11 @@ export const AnimalIntegration = () => {
     rightProfilePhoto: formData?.rightProfilePhoto || null,
     frontPhoto: formData?.frontPhoto || null,
     leftProfilePhoto: formData?.leftProfilePhoto || null,
+    rightProfilePhotoCopy:
+      formData?.rightProfilePhotoCopy || animal?.rightProfilePhotoCopy,
+    frontPhotoCopy: formData?.frontPhotoCopy || animal?.frontPhotoCopy,
+    leftProfilePhotoCopy:
+      formData?.leftProfilePhotoCopy || animal?.leftProfilePhotoCopy,
     unitId: formData.unitId,
     entityId: formData.entityId,
     name: formData.name,
@@ -89,25 +93,19 @@ export const AnimalIntegration = () => {
     try {
       setLoading(true);
 
-      isNew
-        ? await addAnimal(assignCreateProps(mapAnimal(formData)))
-        : await updateAnimal(animal.id, assignUpdateProps(mapAnimal(formData)));
+      const _formData = mapAnimal(formData);
 
       isNew
-        ? await addAnimalLog({
-            id: animal.id,
-            ...formData,
-            rightProfilePhoto: formData?.rightProfilePhotoCopy || null,
-            frontPhoto: formData?.frontPhotoCopy || null,
-            leftProfilePhoto: formData?.leftProfilePhotoCopy || null,
-          })
-        : await updateAnimalLog(animal.id, {
-            id: animal.id,
-            ...formData,
-            rightProfilePhoto: formData?.rightProfilePhotoCopy || null,
-            frontPhoto: formData?.frontPhotoCopy || null,
-            leftProfilePhoto: formData?.leftProfilePhotoCopy || null,
-          });
+        ? await addAnimal(assignCreateProps(_formData))
+        : await updateAnimal(animal.id, assignUpdateProps(_formData));
+
+      // Add animal history
+      if (!animal?.status || animal?.status === "registered") {
+        await addAnimalLog({
+          id: animal.id,
+          ..._formData,
+        });
+      }
 
       await notification({ type: "success" });
       onGoBack();
@@ -156,9 +154,13 @@ const Animal = ({
   loading,
   onGoBack,
 }) => {
-  const [rightProfilePhotoCopy, setRightProfilePhotoCopy] = useState(null);
-  const [frontPhotoCopy, setFrontPhotoCopy] = useState(null);
-  const [leftProfilePhotoCopy, setLeftProfilePhotoCopy] = useState(null);
+  const [rightProfilePhotoCopy, setRightProfilePhotoCopy] = useState(
+    animal?.rightProfilePhotoCopy,
+  );
+  const [frontPhotoCopy, setFrontPhotoCopy] = useState(animal?.frontPhotoCopy);
+  const [leftProfilePhotoCopy, setLeftProfilePhotoCopy] = useState(
+    animal?.leftProfilePhotoCopy,
+  );
 
   const isEquine = animalType === "equine";
   const isCattle = animalType === "cattle";
@@ -295,7 +297,9 @@ const Animal = ({
                             }
                       }
                       onChange={(file) => onChange(file)}
-                      onChangeCopy={(file) => setRightProfilePhotoCopy(file)}
+                      onChangeCopy={(file) =>
+                        file && setRightProfilePhotoCopy(file)
+                      }
                       required={required(name)}
                       error={error(name)}
                     />
@@ -331,7 +335,7 @@ const Animal = ({
                             }
                       }
                       onChange={(file) => onChange(file)}
-                      onChangeCopy={(file) => setFrontPhotoCopy(file)}
+                      onChangeCopy={(file) => file && setFrontPhotoCopy(file)}
                       required={required(name)}
                       error={error(name)}
                     />
@@ -367,7 +371,9 @@ const Animal = ({
                             }
                       }
                       onChange={(file) => onChange(file)}
-                      onChangeCopy={(file) => setLeftProfilePhotoCopy(file)}
+                      onChangeCopy={(file) =>
+                        file && setLeftProfilePhotoCopy(file)
+                      }
                       required={required(name)}
                       error={error(name)}
                     />
