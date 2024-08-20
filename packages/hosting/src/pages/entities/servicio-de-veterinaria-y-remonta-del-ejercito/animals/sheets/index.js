bf11d84cf2b176c2d3a4df819ddd71ae38ed6890
Angel Gala <galafloresangelemilio@gmail.com>
2024-08-20 00:48:09 -0500

    addedsheet family tree

--- packages/hosting/src/pages/entities/servicio-de-veterinaria-y-remonta-del-ejercito/animals/sheets/index.js
+++ packages/hosting/src/pages/entities/servicio-de-veterinaria-y-remonta-del-ejercito/animals/sheets/index.js
@@ -11,6 +11,7 @@ import { useDocumentData } from "react-firebase-hooks/firestore";
 import { animalsRef } from "../../../../../firebase/collections";
 import { AnimalsType } from "../../../../../data-list";
 import { getAnimalEntitiesAndBosses } from "../../../../../utils";
+import { FamilyTreeSheet } from "./FamilyTreeSheet";
 
 export const PdfAnimalRegistrationCardSheet = () => {
   const { animalId } = useParams();
@@ -47,10 +48,29 @@ export const PdfAnimalRegistrationCardSheet = () => {
 
   const animalView = animalMapper(animal);
 
+  const familyTreeView = (animal) =>
+    (animal?.parents || []).map((_animal) => {
+      if (!_animal.parents) return;
+
+      return (
+        <div key={_animal.id} className="item">
+          <div className="item-information">
+            <span>{_animal?.fullName}</span>
+            <span>{_animal?.registrationNumber}</span>
+            <span>{_animal?.raceOrLine}</span>
+          </div>
+          <div className="family-tree-branch">{familyTreeView(_animal)}</div>
+        </div>
+      );
+    });
+
   return (
     <PDF>
       <Sheet layout="landscape">
         <AnimalCardSheet animal={animalView} />
+      </Sheet>
+      <Sheet layout="landscape">
+        <FamilyTreeSheet animal={animalView} familyTreeView={familyTreeView} />
       </Sheet>
     </PDF>
   );
