5e43eb140ba8d8d63c1b185c8cd4248fe38f0694
Angel Gala <galafloresangelemilio@gmail.com>
2024-10-18 18:48:18 -0500

    refactored surveys

--- packages/hosting/src/utils/surveys/getOcupationalGroup.js
+++ packages/hosting/src/utils/surveys/getOcupationalGroup.js
@@ -1,11 +1,10 @@
 import { Surveys } from "../../data-list";
 
 export const getOcupationalGroup = (code, value) => {
-  const questionOptions = Surveys.questions.find(
-    (question) => question.code === code,
-  )?.options;
+  const questionOptions =
+    Surveys.questions.find((question) => question.code === code)?.options || [];
 
-  const options = questionOptions.flatMap((option) => option.options);
+  const options = questionOptions.flatMap((option) => option.options) || [];
 
   return options.find((option) => option.value === value)?.label;
 };
