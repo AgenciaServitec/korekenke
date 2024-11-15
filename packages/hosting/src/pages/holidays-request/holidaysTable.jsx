84de2cd5c4ce59f97fba98511ca7992b4028bf35
Joaquin-Huaranga <joaquinhuarangaorihuela@gmail.com>
2024-11-15 10:00:23 -0500

    Add options states (#446)
    * refix state and options from holidaysTable.jsx.
    waiting for data.
    
    * re-fix code
    
    * format code

--- packages/hosting/src/pages/holidays-request/holidaysTable.jsx
+++ packages/hosting/src/pages/holidays-request/holidaysTable.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import { TableVirtualized } from "../../components";
+import { Acl, IconAction, TableVirtualized } from "../../components";
 import { capitalize } from "lodash";
-import { HolidaysTemps } from "../../data-list";
-import dayjs from "dayjs";
+import { HolidaysRequestStatus, HolidaysTemps } from "../../data-list";
+import { Space, Tag } from "antd";
+import { faEdit, faReply, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export const HolidaysTable = ({ loading }) => {
   const columns = [
@@ -36,7 +37,10 @@ export const HolidaysTable = ({ loading }) => {
       title: "Estado",
       align: "center",
       width: ["7rem", "100%"],
-      render: (added) => added.status,
+      render: (added) => {
+        const holidayStatus = HolidaysRequestStatus[added.status];
+        return <Tag color={holidayStatus?.color}>{holidayStatus?.name}</Tag>;
+      },
     },
     {
       title: "Respuesta",
@@ -48,7 +52,49 @@ export const HolidaysTable = ({ loading }) => {
       title: "Opciones",
       align: "center",
       width: ["8rem", "100%"],
-      render: (added) => "opciones aqui",
+      render: (added) => (
+        <Space>
+          <Acl
+            category="public"
+            subCategory="holidaysRequest"
+            name="/holidays-request#reply"
+          >
+            {added.status !== "finalized" && (
+              <IconAction
+                tooltipTitle="Responder registro"
+                icon={faReply}
+                styled={{ color: (theme) => theme.colors.primary }}
+                onClick={() => console.log("Aun no disponible")}
+              />
+            )}
+          </Acl>
+          <Acl
+            category="public"
+            subCategory="holidaysRequest"
+            name="/holidays-request/:holidayRequestId"
+          >
+            {added?.status !== "finalized" && (
+              <IconAction
+                tooltipTitle="Editar"
+                icon={faEdit}
+                onClick={() => console.log("No disponible")}
+              />
+            )}
+          </Acl>
+          <Acl
+            category="public"
+            subCategory="holidaysRequest"
+            name="/holidays-request#delete"
+          >
+            <IconAction
+              tooltipTitle="Eliminar"
+              icon={faTrash}
+              styled={{ color: (theme) => theme.colors.error }}
+              onClick={() => console.log("Aun no existe")}
+            />
+          </Acl>
+        </Space>
+      ),
     },
   ];
 
--- packages/hosting/src/pages/holidays-request/index.jsx
+++ packages/hosting/src/pages/holidays-request/index.jsx
@@ -5,25 +5,30 @@ import { Acl, Row, Col, Title } from "../../components";
 import { useNavigate } from "react-router";
 
 export const HolidaysRequestIntegration = () => {
-  const navigate = useNavigate();
-
   return <HolidayList />;
 };
 
 const HolidayList = () => {
   return (
-    <Container>
-      <Row gutter={[16, 16]}>
-        <Col span={24}>
-          <div className="header-content">
-            <Title level={3}>Lista de Peticiones de Vacaciones</Title>
-          </div>
-        </Col>
-        <Col span={24}>
-          <HolidaysTable />
-        </Col>
-      </Row>
-    </Container>
+    <Acl
+      category="public"
+      subCategory="holidaysRequest"
+      name="/holidays-request"
+      redirect
+    >
+      <Container>
+        <Row gutter={[16, 16]}>
+          <Col span={24}>
+            <div className="header-content">
+              <Title level={3}>Lista de Peticiones</Title>
+            </div>
+          </Col>
+          <Col span={24}>
+            <HolidaysTable />
+          </Col>
+        </Row>
+      </Container>
+    </Acl>
   );
 };
 
