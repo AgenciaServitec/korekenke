777c476cc1ab235879beeed1a7ba3396a9af23ba
Angel Gala <galafloresangelemilio@gmail.com>
2025-03-10 12:49:25 -0500

    updated whatsapp support number

--- packages/hosting/src/pages/login/index.js
+++ packages/hosting/src/pages/login/index.js
@@ -81,7 +81,7 @@ export const LoginIntegration = () => {
       </div>
       <div className="btn-whatsapp">
         <a
-          href="https://api.whatsapp.com/send?phone=941801827"
+          href="https://api.whatsapp.com/send?phone=51929054672"
           target="_blank"
           rel="noreferrer"
         >
