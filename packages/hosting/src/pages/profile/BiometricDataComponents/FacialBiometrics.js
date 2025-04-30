c3f0315c9003515baabbdc51f4e2d9dced62c7fa
Angel Gala <galafloresangelemilio@gmail.com>
2025-04-30 16:34:10 -0500

    refactored FacialBiometrics and update content tabs

--- packages/hosting/src/pages/profile/BiometricDataComponents/FacialBiometrics.js
+++ packages/hosting/src/pages/profile/BiometricDataComponents/FacialBiometrics.js
@@ -1,26 +1,15 @@
 import React, { useEffect, useState } from "react";
-import {
-  useDevice,
-  useFaceDetection,
-  useFormUtils,
-  useWebcam,
-} from "../../../hooks";
+import { useDevice, useFaceDetection, useWebcam } from "../../../hooks";
 import {
   Button,
   Card,
   Col,
-  Form,
   notification,
   Paragraph,
   Row,
-  Tag,
 } from "../../../components";
 import { useAuthentication, useModal } from "../../../providers";
-import * as yup from "yup";
 import styled from "styled-components";
-import { Controller, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { assign } from "lodash";
 import {
   apiErrorNotification,
   getApiErrorResponse,
@@ -42,55 +31,26 @@ export const FacialBiometrics = () => {
 
   const showAlert = !authUser?.biometricVectors;
 
-  const schema = yup.object({
-    biometricVectors: yup.array().default([]),
-  });
-
-  const {
-    control,
-    formState: { errors },
-    handleSubmit,
-    reset,
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const { error, required } = useFormUtils({ errors, schema });
-
-  const updateProfile = async (formData) => {
+  const updateProfile = async () => {
     try {
-      const response = await putUser(
-        assign({}, formData, {
-          id: authUser.id,
-          phone: authUser.phone,
-          email: authUser.email,
-          biometricVectors: biometricVectors || [],
-        }),
-      );
+      const response = await putUser({
+        id: authUser.id,
+        phone: authUser.phone,
+        email: authUser.email,
+        biometricVectors: biometricVectors || [],
+      });
 
       if (!putUserResponse.ok) {
         throw new Error(response);
       }
+
+      setBiometricVectors([]);
 
       notification({ type: "success" });
     } catch (e) {
       const errorResponse = await getApiErrorResponse(e);
       apiErrorNotification(errorResponse);
     }
-  };
-
-  useEffect(() => {
-    resetForm();
-  }, [authUser]);
-
-  const resetForm = () => {
-    reset({
-      biometricVectors: authUser?.biometricVectors || [],
-    });
-  };
-
-  const onSubmit = async (formData) => {
-    await updateProfile(formData);
   };
 
   const onShowWebcam = () => {
@@ -118,89 +78,57 @@ export const FacialBiometrics = () => {
         style={{ padding: 16 }}
       >
         <Col span={24} style={{ textAlign: "center" }}>
-          {showAlert ? (
-            <Tag
-              className="alert-tag"
-              icon={
-                <FontAwesomeIcon
-                  icon={faCircleExclamation}
-                  style={{ marginRight: 4 }}
-                />
-              }
-            >
-              Registre su Rostro
-            </Tag>
-          ) : (
-            <Tag
-              className="success-tag"
-              icon={
+          <Card
+            className={`status-card ${biometricVectors.length ? "detected-biometric" : authUser.biometricVectors ? "registered-biometric" : "unregistered-biometric"}`}
+          >
+            <div>
+              {biometricVectors.length ? (
                 <FontAwesomeIcon
                   icon={faCircleCheck}
-                  style={{ marginRight: 4 }}
+                  style={{ color: "#78d225", fontSize: 20 }}
                 />
-              }
+              ) : showAlert ? (
+                <FontAwesomeIcon
+                  icon={faCircleExclamation}
+                  style={{ color: "red", fontSize: 20 }}
+                />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faCircleCheck}
+                  style={{ color: "blue", fontSize: 20 }}
+                />
+              )}
+              <Paragraph className="status-paragraph">
+                {biometricVectors.length
+                  ? "Rostro detectado"
+                  : authUser.biometricVectors
+                    ? "Rostro registrado"
+                    : "Rostro no registrado"}
+              </Paragraph>
+            </div>
+          </Card>
+        </Col>
+        {biometricVectors.length ? (
+          <Col span={24} style={{ textAlign: "center", marginBottom: 16 }}>
+            <Button
+              type="primary"
+              size="large"
+              onClick={updateProfile}
+              loading={putUserLoading}
+              disabled={!isDetected}
             >
-              Rostro Registrado
-            </Tag>
-          )}
-        </Col>
-        <Col span={24} style={{ textAlign: "center", marginBottom: 16 }}>
-          <Button type="primary" onClick={onShowWebcam} size="large">
-            {authUser.biometricVectors
-              ? "Cambiar Biométricos"
-              : "Registrar Rostro"}
-          </Button>
-        </Col>
-        <Col span={24}>
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Row gutter={[16, 16]}>
-              <Col span={24}>
-                <Controller
-                  control={control}
-                  name="biometricVectors"
-                  render={() => (
-                    <Card
-                      className={`status-card ${biometricVectors.length ? `has-template` : ""}`}
-                    >
-                      <div>
-                        {biometricVectors.length ? (
-                          <FontAwesomeIcon
-                            icon={faCircleCheck}
-                            style={{ color: "#78d225", fontSize: 20 }}
-                          />
-                        ) : (
-                          <FontAwesomeIcon
-                            icon={faCircleExclamation}
-                            style={{ color: "#c8d32f", fontSize: 20 }}
-                          />
-                        )}
-                        <Paragraph
-                          className={`status-paragraph ${biometricVectors.length ? `has-template-paragraph` : ""}`}
-                        >
-                          {biometricVectors.length
-                            ? "Rostro detectado"
-                            : "Se habilitará el botón al detectar su rostro"}
-                        </Paragraph>
-                      </div>
-                    </Card>
-                  )}
-                />
-              </Col>
-              <Col xs={24} sm={12} md={8} style={{ margin: "0 auto" }}>
-                <Button
-                  type="primary"
-                  size="large"
-                  block
-                  htmlType="submit"
-                  loading={putUserLoading}
-                  disabled={!isDetected}
-                >
-                  Guardar
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        </Col>
+              Guardar
+            </Button>
+          </Col>
+        ) : (
+          <Col span={24} style={{ textAlign: "center", marginBottom: 16 }}>
+            <Button type="primary" onClick={onShowWebcam} size="large">
+              {authUser.biometricVectors
+                ? "Actualizar rostro"
+                : "Registrar rostro"}
+            </Button>
+          </Col>
+        )}
       </Row>
     </FacialContainer>
   );
@@ -259,61 +187,37 @@ const Container = styled.div`
 `;
 
 const FacialContainer = styled.div`
-  .alert-tag {
-    color: #cf1322;
-    background-color: #fff1f0;
-    border: 1px solid #ffccc7;
-    border-radius: 20px;
-    padding: 8px 16px;
-    font-size: 0.9rem;
-    font-weight: 500;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
-  }
-
-  .success-tag {
-    color: #389e0d;
-    background-color: #f6ffed;
-    border: 1px solid #b7eb8f;
-    border-radius: 20px;
-    padding: 8px 16px;
-    font-size: 0.9rem;
-    font-weight: 500;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
-  }
-
   .status-card {
     text-align: center;
     max-width: 400px;
     margin: 0 auto;
-    padding: 16px;
+    padding: 1rem;
     border-radius: 12px;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-    background-color: rgba(200, 211, 47, 0.1);
-    border: 2px solid #c8d32f;
+    border: 2px solid #389e0d;
+    font-size: 0.9rem;
+  }
 
-    svg {
-      font-size: 20px;
-      color: #c8d32f;
-    }
+  .registered-biometric {
+    color: blue;
+    background-color: #e5e5f8;
+    border: 2px solid blue;
+  }
 
-    &.has-template {
-      background-color: rgba(120, 210, 37, 0.1);
-      border-color: #78d225;
+  .unregistered-biometric {
+    color: #cf1322;
+    background-color: #fff1f0;
+    border: 2px solid #cf1322;
+  }
 
-      svg {
-        color: #78d225;
-      }
-    }
+  .detected-biometric {
+    color: #389e0d;
+    background-color: #f6ffed;
   }
 
   .status-paragraph {
-    color: #c8d32f;
     font-weight: 500;
     margin: 0;
     font-size: 14px;
-
-    &.has-template-paragraph {
-      color: #78d225;
-    }
   }
 `;
