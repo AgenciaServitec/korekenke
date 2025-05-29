c1c9cb28260089854ada6d88fe46d2eb9beaa1af
Angel Gala <85372974+AngelDev2001@users.noreply.github.com>
2025-05-29 18:28:29 -0500

    Add search data user (#628)
    * update firestore indexes of assistances
    
    * added searchData in user
    
    * format code

--- packages/hosting/src/pages/register/PersonalInformation.js
+++ packages/hosting/src/pages/register/PersonalInformation.js
@@ -16,7 +16,11 @@ import { useFormUtils } from "../../hooks";
 import styled from "styled-components";
 import { mediaQuery } from "../../styles";
 import { DegreesArmy, BloodGroup } from "../../data-list";
-import { getLocalStorage, setLocalStorage } from "../../utils";
+import {
+  getLocalStorage,
+  getSearchDataToUser,
+  setLocalStorage,
+} from "../../utils";
 import { fetchCollectionOnce } from "../../firebase/utils";
 import { usersRef } from "../../firebase/collections";
 import { LogoPrimary } from "../../images";
@@ -90,6 +94,7 @@ export const PersonalInformation = ({ prev, next, currentStep }) => {
     degree: formData.degree,
     bloodGroup: formData.bloodGroup,
     cgi: formData.cgi,
+    searchData: getSearchDataToUser(formData, formData.firstName),
   });
 
   const onSubmitLogin = async (formData) => {
